85dca61106998917f3a9bdba9439cda97170a4e4
Avinash <avin.roy001@gmail.com>
2024-06-05 08:42:28 +0530

    hashing

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,7 +1,14 @@
 const user = require("../model/user.model")
+const bcrypt=require("bcryptjs");
+
+
+
 
 const creatUser=async(req,res)=>{
-    let d=await user.create(req.body)
+    const{username,password}=req.body;
+    // const saltR=10;
+    // const hashP=await bcrypt.hash(password,saltR)
+    let d=await user.create({username:username,password:password})
     console.log(req.body)
     res.send(d)
 
@@ -9,13 +16,20 @@ const creatUser=async(req,res)=>{
 
 const findUser=async(req,res)=>{
     const{username,password}=req.body
-    let d=await user.findOne({username:username,password:password})
-    console.log(req.body)
+    let d=await user.findOne({username:username})
     if(d){
-        res.send(d)
-    }else{
-        return res.status(401).send("unauthorised")
+        bcrypt.compare(d.password,password,(err,response)=>{
+            console.log(d.password)
+            if(err){
+                res.status(401).json("unauthorised")
+            }else{
+                res.status(200).json("success");
+            }
+        });
     }
+
+   
+   
    
 
 }
--- Backend/model/user.model.js
+++ Backend/model/user.model.js
@@ -1,7 +1,19 @@
 const mongoose=require("mongoose");
+const bcrypt=require("bcryptjs");
 const userSchema=new mongoose.Schema({
     username:String,
     password:String
+})
+userSchema.pre("save", async function(next){
+    console.log("pre",this)
+    if(!this.isModified("password")){
+        next();
+
+    }else{
+        const salt=10;
+        const hash=await bcrypt.hash(this.password,salt);
+        this.password=hash;
+    }
 })
 const user=mongoose.model("user",userSchema);
 module.exports=user;
